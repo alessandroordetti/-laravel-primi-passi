6aef8f87981a6fc6a84a2339e1036cae482715d8
alessandroordetti <alessandro.ordetti@gmail.com>
2022-05-03 16:17:25 +0200

    Bonus

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -13,11 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
-
 Route::get('/', function () {
+    return view('welcome');
+}); 
+
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/options', function () {
+    return view('options');
 });
 
